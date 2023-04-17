5f6ac3a0cd408d6241bbed9a7e5744351c456a66
Muhammad Hussnain Fareed <hasni.rock@yahoo.com>
2023-04-17 11:48:06 +0500

    Add project files.

---
+++ Object Oriented Programming (OOP)/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ObjectOrientedProgramming
+{
+
+    // 1. Classes and Objects
+    class MyClass
+    {
+        public string name;
+        public int age;
+    }
+
+    class ExecuteMyClass
+    {
+        static void Main(string[] args)
+        {
+            MyClass obj = new MyClass();
+            obj.name = "Test";
+            obj.age = 5;
+
+            Console.WriteLine($"My name is {obj.name} and my age is {obj.age}.");            
+        }
+    }
+}
